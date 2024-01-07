3eb08b694b97382174654c59fa78c10493cf427f
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-01-07 01:47:19 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ index file
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -11,3 +12,21 @@ def status():
     """create a route /status on the object app_views
     that returns a JSON"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    retrieves the number of each objects by type
+    """
+    stats_dict = {}
+
+    # Define the object types you want to count
+    object_types = ['User', 'Place', 'City', 'State', 'Amenity', 'Review']
+
+    # Retrieve the count for each object type
+    for obj_type in object_types:
+        count = storage.count(obj_type)
+        stats_dict[obj_type] = count
+
+    return jsonify(stats_dict)
+
